a034ce0f7813d869e8e6dfeb7835c11b011a513b
Misukisti <misukisti@gmail.com>
2025-04-16 00:22:20 +0300

    valmis hover button projekti

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-
-for (let i = 1; i<=5; i++ ) {
-    const button = document.getElementById(`mybutton${i}`);
-    const text1 = document.getElementById(`textToShow${i}`);
-
+// olisi varmaan päässyt helpommalla suoraan css:ään mutta halusin treenata js
+for (let i = 1; i <= 5; i++) {
+    const button = document.getElementById(`myButton${i}`);
+    const text = document.getElementById(`textToShow${i}`);
+  
+    // Alkuasetukset
+    text.style.opacity = 0;
+    text.style.transition = 'opacity 0.6s ease-in';
+  
     button.addEventListener('mouseover', function() {
-        text.style.opacity = 1;
-      });
-    
-      button.addEventListener('mouseleave', function() {
-        text.style.opacity = 0;
-      });
-}
+      text.style.opacity = 1;
+    });
+  
+    button.addEventListener('mouseleave', function() {
+      text.style.opacity = 0;
+    });
+  }
